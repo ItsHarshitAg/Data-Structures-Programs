dc0aa9bd9995dbc34078f48a6a106b134616cb6b
agarwalharshit2990 <agarwalharshit2990@gmail.com>
2024-02-14 19:19:48 +0530

    Adding binary search

---
+++ Binary_Search.py
@@ -0,0 +1,18 @@
+def binary_search(arr, x):
+    # first and last keep track of the search boundaries
+    first = 0
+    last = len(arr) - 1
+    mid = 0
+    while first <= last:
+        mid = (last + first) // 2
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            first = mid + 1
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            last = mid - 1
+        # x is present at mid
+        else:
+            return mid
+    # If we reach here, then the element was not present
+    return -1
